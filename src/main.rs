d9e4276476c03d8e2d134d000ef7ca08cbd85ea4
Jason Durek <durekjason@gmail.com>
2024-01-22 09:42:22 -0500

    Learned basic timer cycles with Timer resource

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,21 @@ struct Name(String);
 
 fn main() {
     App::new()
-    .add_systems(Startup, add_people)
-    .add_systems(Update, (hello_world, (greet_people, update_people).chain()))
+    .add_plugins((DefaultPlugins, HelloPlugin))
     .run();
 }
 
-fn hello_world() {
-    println!("hello world!");
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (greet_people));
+    }
 }
+
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Ford Prefect".to_string())));
@@ -25,9 +32,15 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Marvin Dprs".to_string())));
 }
 
-fn greet_people(query: Query<&mut Name, With<Person>>) {
-    for name in &query {
-        println!("Hello {}!", name.0);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&mut Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("Hello {}!", name.0);
+        }    
     }
 }
 
