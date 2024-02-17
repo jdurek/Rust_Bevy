96445ae772aaeb4c71ef9ee8ac82bd029b3236b1
Jason Durek <durekjason@gmail.com>
2024-02-16 20:19:25 -0500

    Added grid_index logic to determine which 2 grids were involved with a wall update

--- src/minimap/mod.rs
+++ src/minimap/mod.rs
@@ -42,6 +42,7 @@ impl WallGrid {
         }
 
     }
+
     // Helper function to determine 'where' the wall is relative to a line provided
     // Expects the coordinates to be a distance of 1, and no further
     pub fn wall_index(&self, x1:i32, y1:i32, x2:i32, y2:i32) -> Result<usize, String> {
@@ -98,7 +99,7 @@ impl WallGrid {
         }
         else {
             // Wall index was invalid (Not within the bounds of the map)
-            // Don't display wall
+            // Do error handling here later if we need to
         }
     }
     
@@ -107,7 +108,7 @@ impl WallGrid {
     pub fn remove_wall(&mut self, x1:i32, y1:i32, x2:i32, y2:i32){
         if let Ok(wall_loc) = self.wall_index(x1, y1, x2, y2){
             // Update entry in WallGrid
-            self.walls[wall_loc] = Wall {vis:false, pres: false};
+            self.walls[wall_loc] = Wall {vis:true, pres: false};
         }
         else {
             // Wall index was invalid (Not within the bounds of the map)
@@ -132,14 +133,122 @@ impl MapGrid {
         }
     }
 
-    // Translate (x,y) coordinate into Vector index
-    pub fn xy_index(&self, x:i32, y:i32) -> usize {
-        (y as usize * self.dim_x as usize) + x as usize
+    // Translate (x,y) coordinate into Vector index directly
+    // Potential TODO - add error handling so we can just call this directly?
+    pub fn xy_index(&self, x:i32, y:i32) -> i32 {
+        (y * self.dim_x ) + x
     }
 
     // Given a line of 2 points, figure out which 1-2 grids are involved
+    pub fn grid_index(&self, x1:i32, y1:i32, x2:i32, y2:i32) -> Result<[i32;2], String> {
+
+        // Validate the line length
+        if x1-x2 + y1-y2 != 1 {
+            // Distance is not equal to 1 - cannot use this line
+            return Err(String::from("Invalid Line"))
+        }
+        if x1 > self.dim_x + 1 || x2 > self.dim_x + 1 || x1 < 0 || x2 < 0 
+         || y1 > self.dim_y + 1 || y2 > self.dim_y + 1 || y1 < 0 || y2 < 0 {
+            // Start or End coordinate is outside of our map
+            return Err(String::from("Coordinate out of bounds"))
+        }
+
+        // Line is valid - translate into our Vector
+        let x_diff = x1 - x2;
+        let y_diff = y1 - y2;
+        let mut index = [0 , 0];
+        // Index array is either [Top Cell, Bottom Cell] or [Left Cell, Right Cell] indexes
+        // Value of -1 means there is no associated cell available
+        match (x_diff, y_diff) {
+            (-1, 0) => { // Leftward - use left end (x2,y2) as index coordinate
+                if y2 == 0 {            // Bottom edge of the map
+                    index[0] = -1;
+                } else {
+                    index[0] = self.xy_index(x2, y2-1);
+                }
+
+                if y2 == self.dim_y {   // Top edge of the map
+                    index[1] = -1;
+                } else {
+                    index[1] = self.xy_index(x2, y2);
+                }
+            }, 
+            (1, 0) => { // Rightward - use right end (x1,y1) as index coordinate
+                if y1 == 0 {            // Bottom edge of the map
+                    index[0] = -1;
+                } else {
+                    index[0] = self.xy_index(x1, y1 -1);
+                }
+
+                if y1 == self.dim_y {   // Top edge of map
+                    index[1] = -1;
+                } else {
+                    index[1] = self.xy_index(x1, y1);
+                }
+            }, 
+            (0, -1) => { // Downward - use bottom end (x2,y2) as index coordinate
+                if x2 == 0 {            // Left edge of the map
+                    index[0] = -1;
+                } else {
+                    index[0] = self.xy_index(x2-1, y2);
+                }
+
+                if x2 == self.dim_x {   // Right edge of the map
+                    index[1] = -1;
+                } else {
+                    index[1] = self.xy_index(x2, y2);
+                }
+            },
+            (0, 1) => { // Upward - use bottom end (x1, y1) as index coordinate 
+                if x1 == 0 {            // Left Edge of map
+                    index[0] = -1;
+                } else {
+                    index[0] = self.xy_index(x1-1, y1);
+                }
+
+                if x1 == self.dim_x {   // Right edge of map
+                    index[1] = -1;
+                } else {
+                    index[1] = self.xy_index(x1, y1);
+                }
+            }, 
+            _ => {
+                return Err(String::from("Unknown Match case in MapGrid"))
+            }
+        };
+        Ok(index)
+    }
 
     // Given a line of 2 points, add 'walls' to the relavant grid entries
+    pub fn add_walls(&mut self, x1:i32, y1:i32, x2:i32, y2:i32){
+        // Fetch the grid index values - use that to figure out which walls were changed
+        if let Ok(grids) = self.grid_index(x1, y1, x2, y2){
+            // Since the grid_index function returned fine, we don't need to validate
+            let x_diff = x1 - x2;
+            let y_diff = y1 - y2;
+            match (x_diff, y_diff) {
+                (-1, 0) => {    // Leftward - bottom cell, top cell
+                    // TODO - Insert new walls tuple into Tile Entity for the 4 cases here
+                    // This will probably run into mutability issues, but I don't want to rewrite the tile every time...
+                },
+                (1,0) => {      // Rightward - bottom cell, top cell
+
+                }
+                (0, -1) => {    // Upward - left cell, right cell
+
+                }
+                (0,1) => {      // Downard - left cell, right cell
+
+                }
+                _ => { } // Blank, since we've already handled it
+            }
+
+        }
+        else {
+            // Error handling for some issue with the walls provided
+        }
+        
+    }
 
     // Given a line of 2 points, remove 'walls' from the relevant grid entries
 }
