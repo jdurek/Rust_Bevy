1ee65c19ce598a2681699edcd9b2fcca8d729a04
Jason Durek <durekjason@gmail.com>
2024-02-19 09:03:52 -0500

    Updated so the line spawns on click, drag modifies it, and release despawns it

--- src/minimap/draw_map.rs
+++ src/minimap/draw_map.rs
@@ -3,8 +3,15 @@
     Also handles some setup needed for the UI drawing
 */
 
+use bevy::ecs::world;
+
 use crate::minimap::*;
 
+// Component is only for query lookup
+#[derive(Component)]
+pub struct DragLine;
+#[derive(Component)]
+pub struct LineCoords;
 
 // Draws only the grid
 pub fn draw_grid(mut commands: Commands, mg: Res<MapGrid>) {
@@ -83,6 +90,7 @@ pub fn mouse_wall_gui(
     mouse: Res<Input<MouseButton>>, 
     // Reference to our Camera so we can translate to world coordinates
     map_cam: Query<(&Camera, &GlobalTransform)>,  //TODO - adjust when more cameras are added
+    mut draw_line: Query<(&DragLine, &mut Transform, Entity)>,
 ) {
     // Fetch camera information
     let (camera, camera_transform) = map_cam.single();
@@ -106,6 +114,11 @@ pub fn mouse_wall_gui(
         // Display mouse coordinates VIA a textbox 
         let pos_str = format!("({loc_x}, {loc_y})");
 
+        // TODO - Check if the sprites exists or not
+        // If they do, we're probably in the pressed or just_released states - handle using the sprite we queried
+        // If they don't, handle accordingly 
+
+        // Mouse has been pressed - spawn in 2 sprites (Line itself, and the coords)
         if mouse.just_pressed(MouseButton::Left) {
             // Try to find nearest whole coordinate within reason (EG if a pixel is 16 tiles, 4 tiles near the corner)
             if (loc_x / ZOOM_LEVEL < 0.2 || loc_x / ZOOM_LEVEL > 0.8) &&
@@ -114,44 +127,67 @@ pub fn mouse_wall_gui(
                 println!("Close enough to a corner!");
                 start_x = world_position.x;
                 start_y = world_position.y;
+
+                // TODO - add a component to these bundles specifically for the query
+                commands.spawn(
+                    TextBundle::from_section(
+                        pos_str,
+                        TextStyle {
+                            ..Default::default()
+                            },
+                        )
+                        .with_text_alignment(TextAlignment::Right)
+                        .with_style(Style {
+                            position_type: PositionType::Absolute,
+                            bottom: Val::Px(5.0),
+                            right: Val::Px(5.0),
+                            ..default()
+                        })
+                        .with_background_color(Color::BLACK)
+                );
+    
+                // Spawn a 'line' with a default rotation and length of 0
+                // Snaps the start of the line to the nearest valid corner
+                                
+                commands.spawn((SpriteBundle{
+                    sprite: Sprite { color: Color::ANTIQUE_WHITE, custom_size: (Some(Vec2::new(1.0,1.0))), ..Default::default() },
+                    visibility: Visibility::Visible,
+                    transform: Transform {
+                        translation: Vec2::new(start_x.round() as f32, start_y.round() as f32).extend(5.0),
+                        scale: Vec3::new(0., 1.5, 1.),
+                        rotation: Quat::from_rotation_z(0.),
+                        ..default()
+                    },
+                    ..Default::default()
+                }, DragLine));
+                
+                
             }
             
             println!("Position: {},{}", loc_x, loc_y);
         }
 
         if mouse.pressed(MouseButton::Left) {
-            // Update the sprite to follow the current position
-            // For testing - show the 'coordinate' the mouse is currently at
-            // TODO - replace with something like a 'score' board where we only edit a field instead of making a new sprite
-            commands.spawn(
-        TextBundle::from_section(
-            pos_str,
-            TextStyle {
-                ..Default::default()
-                },
-            )
-            .with_text_alignment(TextAlignment::Right)
-            .with_style(Style {
-                position_type: PositionType::Absolute,
-                bottom: Val::Px(5.0),
-                right: Val::Px(5.0),
-                ..default()
-            })
-            .with_background_color(Color::BLACK)
-            );
+            // Fetches the sprite to edit (If it exists - it may have not been created)
+            for (drag, mut transf, ent) in draw_line.iter_mut(){
+                // println!("Dumping transform data... {}, {}, {}", transf.translation, transf.scale, transf.rotation)
+                
+                // Updates 2 values in the Transform section of the sprite bundle - Scale and Rotation
+                // Needs to use the Translation values to fetch the 'point of origin' for some computations
+                let norm_pts = (world_position.x - transf.translation.x, world_position.y - transf.translation.y);
+                let theta = norm_pts.1.atan2(norm_pts.0);
+                // Compute dist value - we can assume it will always be a right angle triangle
+                let dist = ((world_position.x - transf.translation.x).abs().powi(2) + (world_position.y - transf.translation.y).abs().powi(2)).sqrt();
 
-            // Spawn a 'line' going from the initially clicked spot to here, and 'remove' the previous line
-            commands.spawn(SpriteBundle{
-                sprite: Sprite { color: Color::ANTIQUE_WHITE, custom_size: (Some(Vec2::new(1.0,1.0))), ..Default::default() },
-                visibility: Visibility::Visible,
-                transform: Transform {
-                    translation: Vec2::new(start_x as f32, start_y as f32).extend(0.0),
-                    scale: Vec3::new(ZOOM_LEVEL, 1.5, 1.),
-                    rotation: Quat::from_rotation_z(1.2),
-                    ..default()
-                },
-                ..Default::default()
-            });
+                // Update our Transform values
+                transf.scale.x = dist;
+                transf.rotation = Quat::from_rotation_z(theta);
+
+            }
+
+
+            // TODO - replace with something like a 'score' board where we only edit a field instead of making a new sprite
+            
         }
         // if mouse.just_pressed(MouseButton::Right) {
         //     // Try to find the nearest 'wall' - just check if we're near an int and take other 2 values
@@ -160,9 +196,13 @@ pub fn mouse_wall_gui(
 
     }   
 
+    // Handle despawning the line entity we just drew - 
     if mouse.just_released(MouseButton::Left) {
-        // Clean up the display (Delete the text_bundle)
-        // Figure out if the new 'wall' is valid - wall_index
+        // Clean up the display 
+        for a in draw_line.iter(){
+            // Despawn the entity - should only be the one anyway
+            commands.entity(a.2).despawn();
+        }
 
         // Update WallGrid (Which is a resource, add to our function's queries) - add_wall
 
